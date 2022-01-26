e16d9ea01c17888c79bb93bd7f4d86faa70e1c4d
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-01-26 12:09:28 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -92,11 +92,11 @@ def callback():
         users_email = userinfo_response.json()["email"]
         picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["name"]
-        return users_name
         if not users_email.endswith("@my.sbunified.org") and not users_email.endswith("@sbunified.org"):
             return "User email not in domain", 401
     else:
         return "User email not available or not verified by Google.", 400
+    return users_name
     return redirect(url_for("render_login"))
 
 def get_google_provider_cfg():
