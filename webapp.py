d778949c223cdab074149e503ac1146b4315e429
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-01-31 13:49:44 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -92,7 +92,7 @@ def callback():
         picture = userinfo_response.json()['picture']
         users_name = userinfo_response.json()['name']
         if not users_email.endswith('@my.sbunified.org') and not users_email.endswith('@sbunified.org'):
-            return render_template('login.html', log_in_error = "Please use your school issued email")
+            return redirect(url_for('login.html', log_in_error = "Please use your school issued email"))
     else:
         return render_template('login.html', log_in_error = "Email not available or verified")
     session['unique_id'] = unique_id
