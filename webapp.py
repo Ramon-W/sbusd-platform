6d5834d535cde837aeeda9ffb9e99b02cf5c8497
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-02-01 13:14:39 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -54,7 +54,6 @@ def login():
         redirect_uri=request.base_url + '/callback',
         scope=['openid', 'email', 'profile'],
         prompt='consent'
-        hd='my.sbunified.org'
     )
     return redirect(request_uri)
 
