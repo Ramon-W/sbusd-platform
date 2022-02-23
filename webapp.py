fec98059923a0a5831b2e95c68f4ba9dd923e39f
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-02-23 15:20:24 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -123,17 +123,15 @@ def logout():
 @socketio.on('connect')
 def connect():
     sys.stdout.write('WORKED')
-    app.logger.info('Worked')
 
 @socketio.on('join_room')
-def join(data): #use .get for python dictionary.
+def join(data):
     #app.logger.info('{} has joined room {}'.format(data[username], data[room]))
     join_room(data['room'])
     socketio.emit('join_room_announcement', data)
 
 @app.route('/sbhs')
 def render_main_page():
-    sys.stdout.write('WORKED')
     socketio.emit('join_room_announcement', {'username': 'bob', 'room': '1'})
     return render_template('index.html')
 
