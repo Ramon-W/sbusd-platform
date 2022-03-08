5964d0950c1c3161cb2d23e65f556ec47f4b8560
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-07 20:01:27 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -130,7 +130,7 @@ def join(data):
 @app.route('/sbhs')
 def render_main_page():
     
-    return render_template('index.html', username = session['users_name'], room = "1")
+    return render_template('index.html', 'username' = session['users_name'], 'room' = '1')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
