74dca19ef5d01a9cfceae29112b790b33da6c567
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-26 12:58:12 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+  $(".space").click(function() {
+    $.ajax({
+    	type : "POST",
+     	url : '/space' + '/123456789',
+     	dataType: "json",
+    	data: JSON.stringify() //(you can put in a variable in here to send data with the request),
+    	contentType: 'application/json;charset=UTF-8',
+    	success: function (data) {
+		console.log(data);
+		}
+	});
+  });
+});
