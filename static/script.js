0ffb5305f700671f56da1ed8e90985b463cdead6
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-26 14:04:50 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(".space").click(function() {
     $.ajax({
-    	type: "GET",
-     	url: '/space',
-     	dataType: "json",
-    	data: JSON.stringify([{'space_id': 'test'}]), //JSOn.stringify
-    	contentType: 'application/json;charset=UTF-8',
-			success: function(result) {
+      type: "POST",
+      url: '/space',
+      dataType: "json",
+      data: JSON.stringify([{'space_id': 'test'}]), //JSOn.stringify
+      contentType: 'application/json;charset=UTF-8',
+      success: function(result) {
         alert(result);
       } 
     });
