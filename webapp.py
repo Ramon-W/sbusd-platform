74ab681bddd0c5bb7bddde99b05be333d8d7e196
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-26 14:14:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -156,10 +156,10 @@ def render_main_page():
 
 @app.route('/space', methods=['GET', 'POST'])#/<space_id>')
 def render_space():
-    if request.method == 'POST':
+    if request.method == 'GET':
         results = {'processed': 'true'}
         return jsonify(results)
-    #return render_template('index.html', username = session['users_name'], room = '1')
+    return render_template('index.html', username = session['users_name'], room = '1')
 
 if __name__ == '__main__':
     socketio.run(app, debug=False)
