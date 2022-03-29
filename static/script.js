51e1eb6e22ead153f1692b9e78100d7c6e2602a4
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-28 22:15:40 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
       url: '/chat_history',
       contentType: 'application/json;charset=UTF-8',
       success: function(chat_history) {
-        chat_history = JSON.parse(chat_history)
+        chat_history = JSON.stringify(chat_history)
         chat_history.forEach(function(item, index) {
           alert(item)
         });
