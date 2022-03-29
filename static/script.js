f9ce4b3e365988aab3e118f938fa051351a80abb
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-28 21:13:09 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
     $.ajax({
       type: "POST",
       url: '/chat_history',
-      dataType: "json",
       contentType: 'application/json;charset=UTF-8',
       success: function(chat_history) {
         alert(JSON.stringify(chat_history));
