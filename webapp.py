123489b8d883c04e1fb8d0d954124d923d28bac8
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-28 21:29:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ import requests
 from flask_talisman import Talisman
 
 from bson.objectid import ObjectId
-#from bson.json_util import dumps
+from bson.json_util import dumps
 
 #import pprint
 #import sys
@@ -165,7 +165,7 @@ def render_main_page():
 @app.route('/chat_history', methods=['GET', 'POST'])
 def chat_history():
     if request.method == 'POST':
-        chat_history = list(collection_messages.find({'room': '1'}))
+        chat_history = dumps(list(collection_messages.find({'room': '1'})))
         return jsonify(chat_history)
 
 @app.route('/space', methods=['GET', 'POST'])#/<space_id>')
