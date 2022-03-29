f981ba6205c854239f8edf5dffba2e24ca4f2744
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-28 21:10:23 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -11,4 +11,15 @@ $(document).ready(function(){
       } 
     });
   });
+  $("#information").click(function() {
+    $.ajax({
+      type: "POST",
+      url: '/chat_history',
+      dataType: "json",
+      contentType: 'application/json;charset=UTF-8',
+      success: function(chat_history) {
+        alert(JSON.stringify(chat_history));
+      } 
+    });
+  });
 });
