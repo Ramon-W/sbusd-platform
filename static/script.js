1314da62296c56040353c052aec10a6b91550064
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-03-29 20:58:51 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -11,20 +11,4 @@ $(document).ready(function(){
       } 
     });
   });
-  socket.on('load_chat_history', function() {
-    $.ajax({
-      type: "POST",
-      url: '/chat_history',
-      contentType: 'application/json;charset=UTF-8',
-      success: function(chat_history) {
-        chat_history.forEach(function(item) {
-          const newMode = document.createElement('div');
-          newMode.className = 'message';
-          newMode.innerHTML = '<b>' + item.name + ':</b> ' + item.message + item.picture + item.datetime;
-          document.getElementById('messages').appendChild(newMode);
-        });
-        //alert(JSON.stringify(chat_history));
-      } 
-    });
-  });
 });
