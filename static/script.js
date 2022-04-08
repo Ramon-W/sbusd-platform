bb0e64c38c8b230aa1251e14d86024b47fc746ec
Ponmo <77691125+Ponmo@users.noreply.github.com>
2022-04-07 17:35:15 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
     textarea.style.height = "";
     textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
   }; 
+  function handleEnter(evt) {
+    if (evt.keyCode == 13 && evt.shiftKey) {
+        if (evt.type == "keypress") {
+            pasteIntoInput(this, "\n");
+        }
+        evt.preventDefault();
+    }
+  }
+  $("#message_input").keydown(handleEnter).keypress(handleEnter);
   $(".space").click(function() {
     $.ajax({
       type: "POST",
