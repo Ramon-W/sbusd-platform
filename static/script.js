bc4926f0c2269436f9b3ff8df47d952c8bf4138c
Ponmo <77691125+Ponmo@users.noreply.github.com>
2022-04-09 15:29:51 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,34 +5,13 @@ $(document).ready(function(){
     textarea.style.height = "";
     textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
   }; 
-function pasteIntoInput(el, text) {
-    el.focus();
-    if (typeof el.selectionStart == "number"
-            && typeof el.selectionEnd == "number") {
-        var val = el.value;
-        var selStart = el.selectionStart;
-        el.value = val.slice(0, selStart) + text + val.slice(el.selectionEnd);
-        el.selectionEnd = el.selectionStart = selStart + text.length;
-    } else if (typeof document.selection != "undefined") {
-        var textRange = document.selection.createRange();
-        textRange.text = text;
-        textRange.collapse(false);
-        textRange.select();
+  $("#message_input").keypress(function (e) {
+    if(e.which === 13 && !e.shiftKey) {
+        e.preventDefault();
+    
+        $(this).closest("form").submit();
     }
-}
-
-function handleEnter(evt) {
-    if (evt.keyCode == 13 && evt.shiftKey) {
-        if (evt.type == "keypress") {
-            pasteIntoInput(this, "\n");
-        }
-        evt.preventDefault();
-    }
-}
-
-// Handle both keydown and keypress for Opera, which only allows default
-// key action to be suppressed in keypress
-$(textarea).keydown(handleEnter).keypress(handleEnter);
+  });
   $(".space").click(function() {
     $.ajax({
       type: "POST",
