ff0e01fe32a2d21b32e75bf0de0445501c1f6e81
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2022-04-15 22:32:00 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     chatDiv.scrollTop = chatDiv.scrollHeight; 
   }
   $('#chat').scroll(function() { //$('#chat').scrollTop() + $('#chat').height();
-    if ($('#chat').scrollTop() + $('#chat').scrollHeight() >= $('#chat').scrollHeight() - 20) {
+    if (Math.abs($('#chat').scrollTop()) == $('#chat').scrollHeight()) {
         alert('top of the div');
     }
   });
