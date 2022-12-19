5c94471d229ee877bdf59e91464835021a00c91d
Ramon Wang <77691280+Ramon-W@users.noreply.github.com>
2022-12-18 18:08:15 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -549,7 +549,7 @@ def create_space():
         return 'expired', 200
     user = collection_users.find_one({"_id": session['unique_id']})
     if request.method == 'POST':
-        if user['owns'] < 3:
+        if user['owns'] > 3:
             return Response(dumps({'success': false}), mimetype='application/json')
         space_id = ObjectId()
         room_id = ObjectId()
